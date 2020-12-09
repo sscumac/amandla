c7c782cb585f8c6475b4bfd4b0c61943e6e37c9f
ricky schoeman <schoeman.ricky@gmail.com>
2020-12-09 18:16:50 +0100

    geocoding and mapbox added

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,26 @@
 class PlacesController < ApplicationController
   def index
     @places = Place.all
+
+    @markers = @places.geocoded.map do |place|
+      {
+        lat: place.latitude,
+        lng: place.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { place: place }),
+        image_url: helpers.asset_url('https://res.cloudinary.com/dpnjiruwh/image/upload/v1607530616/download_ahvevg.png')
+      }
+    end
   end
 
   def show
     @place = Place.find(params[:id])
+
+    @markers = [{
+      lat: @place.latitude,
+      lng: @place.longitude,
+      infoWindow: render_to_string(partial: "info_window", locals: { place: @place }),
+      image_url: helpers.asset_url('https://res.cloudinary.com/dpnjiruwh/image/upload/v1607530616/download_ahvevg.png')
+    }]
   end
 
   def new
