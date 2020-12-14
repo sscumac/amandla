d63296db4e983ecc7431cedca8066a91f98befa4
Sebastian Schumacher <sebastian.schumacher@hotmail.de>
2020-12-14 18:23:23 +0100

    Q&A Controllers and Views added - basically working

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,32 @@
+class QuestionsController < ApplicationController
+
+  def index
+    # @pending_questions = current_user.pending_questions
+    @questions = Question.all
+
+  end
+
+  def new
+    @place = Place.find(params[:place_id])
+    @question = Question.new
+  end
+
+  def create
+    @place = Place.find(params[:place_id])
+    @question = Question.new(question_params)
+    @question.place = @place
+    @question.user = current_user
+    if @question.save
+      redirect_to place_path(@place), notice: "Question succesfully asked"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:content)
+  end
+
+end
